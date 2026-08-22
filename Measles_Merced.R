97d74cc27d2e2e399a52f99730d80654e1eba20d
Ethan <ejacobson494@gmail.com>
2026-08-21 20:05:44 -0700

    basic measles graphic

--- Measles_Merced.R
+++ Measles_Merced.R
@@ -1,9 +1,17 @@
 
 
 
-measles <- read.table("measles_data.txt", header = TRUE)
+measles <- read.table("measles_data.txt", header = TRUE, sep = ',')
 
 # names(measles)
 
-# str(measles)
+measles
 
+# update the date data to fill in zero detection dates 
+
+measles$Date <- as.Date(measles$Date) 
+
+names(measles)
+
+
+plot(measles$Concentration ~ measles$Date, type = 'b') 
