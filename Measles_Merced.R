64438c36cdb03db84990cb5ebef9f939dc9e2181
Ethan <ejacobson494@gmail.com>
2026-08-21 20:24:12 -0700

    finished graphic

--- Measles_Merced.R
+++ Measles_Merced.R
@@ -14,4 +14,11 @@ measles$Date <- as.Date(measles$Date)
 names(measles)
 
 
-plot(measles$Concentration ~ measles$Date, type = 'b') 
+plot(measles$Concentration ~ measles$Date, 
+    type = 'b', 
+    main = 'Measles Detections Merced',  
+    xlab = 'Detection Date', 
+    ylab = 'Concentration (counts/g)', 
+    pch = 19,
+    col = '#3e9499' 
+    ) 
